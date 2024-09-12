06642cbc65213f46f63dceb3956994ce03d850fe
Sumon roy <144287654+Sumon8389@users.noreply.github.com>
2024-09-12 17:14:21 +0530

    Update is.py

--- is.py
+++ is.py
@@ -160,7 +160,7 @@ def crack(uid, pww, total_idz):
                 'sec-fetch-dest': 'empty',
                 'sec-fetch-mode': 'cors',
                 'sec-fetch-site': 'same-origin',
-                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36',
+                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36',
                 'x-asbd-id': '129477',
                 'x-csrftoken': csrftoken,
                 'x-ig-app-id': '936619743392459',
